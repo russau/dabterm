187dce86a2c354d9bea619aef7c32b31f7a86d63
Russ Sayers <russell.sayers@gmail.com>
2019-01-19 14:33:26 -0800

    cosmetic changes to make the terminal "look right"

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
 const Photon = require('electron-photon');
 
 
-const Terminal = require('xterm').Terminal;
-const _debounce = require('lodash.debounce');
-const fit = require('xterm/lib/addons/fit/fit');
-Terminal.applyAddon(fit);
+window.addEventListener("load", function() {
 
-const pty = require('node-pty-prebuilt');
+  const Terminal = require('xterm').Terminal;
+  const _debounce = require('lodash.debounce');
+  const fit = require('xterm/lib/addons/fit/fit');
+  Terminal.applyAddon(fit);
 
-const term = new Terminal({
-  fontFamily: 'mono, courier-new, courier, monospace',
-  fontSize: 16
+  const pty = require('node-pty-prebuilt');
+
+  const term = new Terminal({
+    fontFamily: 'mono, courier-new, courier, monospace',
+    fontSize: 16
+  });
+
+  term.open(document.getElementById('term'));
+  term.fit();
+
+  const ptyProc = pty.spawn('/bin/bash', [], {
+      cols: term.cols,
+      rows: term.rows
+  });
+
+  term.on('data', function(data) {
+      ptyProc.write(data);
+  });
+
+  ptyProc.on('data', function(data) {
+      term.write(data);
+  });
+
+  term.on('resize', function(size) {
+      ptyProc.resize(
+          Math.max(size ? size.cols : term.cols, 1),
+          Math.max(size ? size.rows : term.rows, 1)
+      );
+  });
+
+  fitDebounced = _debounce(() => {
+      term.fit();
+  }, 17);
+
+  window.onresize = () => {
+      fitDebounced();
+  };
+
 });
-
-term.open(document.getElementById('term'));
-term.fit();
-
-const ptyProc = pty.spawn('/bin/bash', [], {
-    cols: term.cols,
-    rows: term.rows
-});
-
-term.on('data', function(data) {
-    ptyProc.write(data);
-});
-
-ptyProc.on('data', function(data) {
-    term.write(data);
-});
-
-term.on('resize', function(size) {
-    ptyProc.resize(
-        Math.max(size ? size.cols : term.cols, 1),
-        Math.max(size ? size.rows : term.rows, 1)
-    );
-});
-
-fitDebounced = _debounce(() => {
-    term.fit();
-}, 17);
-
-window.onresize = () => {
-    fitDebounced();
-};
