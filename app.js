5037e9688fcbdb5ac64f4d26c1be9dd77804bd8a
Russ Sayers <russell.sayers@gmail.com>
2019-01-20 20:51:26 -0800

    very very early test of tabbing

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const Photon = require('electron-photon');
+const Terminal = require('xterm').Terminal;
+const _debounce = require('lodash.debounce');
+const fit = require('xterm/lib/addons/fit/fit');
+Terminal.applyAddon(fit);
+const pty = require('node-pty-prebuilt');
+var Docker = require('dockerode');
+var DockerEvents = require('docker-events');
 
-
-window.addEventListener("load", function() {
-
-  const Terminal = require('xterm').Terminal;
-  const _debounce = require('lodash.debounce');
-  const fit = require('xterm/lib/addons/fit/fit');
-  Terminal.applyAddon(fit);
-
-  const pty = require('node-pty-prebuilt');
-
+function addTerminal(element, exec) {
   const term = new Terminal({
     fontFamily: 'mono, courier-new, courier, monospace',
     fontSize: 16
   });
-
-  term.open(document.getElementById('term'), true);
+  term.open(element, true);
   term.fit();
   term.focus();
 
-  const ptyProc = pty.spawn('/bin/bash', [], {
+  const ptyProc = pty.spawn(exec, [], {
       cols: term.cols,
       rows: term.rows
   });
@@ -46,29 +43,72 @@ window.addEventListener("load", function() {
   window.onresize = () => {
       fitDebounced();
   };
+}
 
-  $('.btn').on('click', function() {
+
+window.addEventListener("load", function() {
+
+  var pane = $('<div/>').addClass("pane")
+          .appendTo('ph-window');
+  var newTerm = $('<div/>').addClass("terminal").appendTo(pane);
+  addTerminal(newTerm.get(0), '/bin/bash');
+
+  var addTab = function() {
+    var tab = $(this).text();
     const myNewTab = document.getElementsByTagName("tab-group")[0].addTab({
-      position: "last", // "last" or "first"
-      closeBtn: true, // Wether the tab can be closed using a button within it
-      isActive: true, // Wether the tab item will be the active one
-      animated: true, // Wether an animation will be shown when adding the tab item
-      content: document.createTextNode($(this).text()) // Node or string
+      position: "last",
+      closeBtn: true,
+      isActive: true,
+      animated: true,
+      content: document.createTextNode(tab) // Node or string
+    });
+    // hide all terminals and create a new one
+    $('.pane').css('display', 'none');
+    var pane = $('<div/>').addClass("pane")
+            .appendTo('ph-window');
+    var newTerm = $('<div/>').addClass("terminal").appendTo(pane);
+    addTerminal(newTerm.get(0), '/bin/bash');
+  };
+  const tabGroup = document.getElementsByTagName("tab-group")[0];
+  tabGroup.addEventListener("tabActivate", function(event) {
+    var tabPos = $('tab-item').index(event.target);
+    $('.pane').each(function( index ) {
+      if (tabPos == index) {
+        $(this).show();
+        // this should give the terminal focus
+      } else {
+        $(this).hide();
+      }
     });
   });
+  tabGroup.addEventListener("tabClose", function(event) {
+    console.log(event);
+  });
+  tabGroup.addEventListener("tabMove", function(event) {
+    console.log(event);
+  });
+  tabGroup.addEventListener("tabAdd", function(event) {
+    console.log(event);
+  });
 
-  var Docker = require('dockerode');
-  var DockerEvents = require('docker-events');
-  var stream = require('stream');
+
 
   var docker = new Docker({
+    // shoud this path be passed as a parameter with
+    // a default override
     socketPath: '/var/run/docker.sock'
   });
 
   docker.listContainers(function (err, containers) {
     containers.forEach(function (containerInfo) {
       var name = containerInfo.Names[0];
-      $('<button/>').addClass("btn btn-default").text(name).appendTo('btn-group');
+      // <button class="btn btn-default">
+      $('<button/>').addClass("btn btn-default")
+                    .text(name.replace("/", ""))
+                    .appendTo('btn-group')
+                    .on("click", addTab);
+                    // shouldn't have to do the event here
+                    // $('btn-group').on( "click", "button.btn", isn't working!
     });
   });
 
